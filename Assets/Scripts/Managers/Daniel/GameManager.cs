2f76dda624bcfd8c3a23e71c47dfa77d06c16dd6
holidayfanboy <mkdaniel2020@gmail.com>
2025-05-31 20:57:52 +0900

    heartui

--- Assets/Scripts/Managers/Daniel/GameManager.cs
+++ Assets/Scripts/Managers/Daniel/GameManager.cs
@@ -1,25 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public int health;
+    private int maxHealth;
     public PlayerSceneTrigger playerScene;
     public SceneManager sceneManager;
+    public Slider slider;
+    public Sprite emptyHeart;
+    public Sprite fullHeart;
+    public Image[] hearts;
 
     void Awake()
     {
+        maxHealth = health;
+        slider.maxValue = maxHealth;
+        slider.value = health;
     }
 
     void Update()
     {
-        
+        slider.value = health;
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+        }
     }
 
     public void HealthDown()
     {
-        if (health > 0)
+        if (health > 1)
         {
             health--;
             Debug.Log("Player Health: " + health);
@@ -30,7 +51,7 @@ public class GameManager : MonoBehaviour
             Debug.Log("Wasted");
             playerScene.SmallEnemyMoveReset();
             sceneManager.Respawn();
-            health = 1;
+            health = 5;
         }
     }
 }
